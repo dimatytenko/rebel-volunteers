789a46a2036dd78ab762e20e2826e233be6fd2a7
Dima Tytenko <amgpetronass@gmail.com>
2024-01-13 18:38:04 +0200

    update blockList component

--- src/containers/Join/index.tsx
+++ src/containers/Join/index.tsx
@@ -10,16 +10,21 @@ import { helpInformList } from './helpInform';
 import { helpMoreList } from './helpMore';
 import { donor } from './donor';
 import { kit } from './firstAidKit';
-import { list } from '../ListOfNecessary';
+// import { list } from '../ListOfNecessary';
 import { useLanguage } from '../../hooks/language';
+import { useOveral } from '../../hooks/overal';
+import img_1 from './assets/necessary/img_1.jpg';
+import img_2 from './assets/necessary/img_2.jpg';
+import img_3 from './assets/necessary/img_3.jpg';
 
 const Join = () => {
   const { t } = useTranslation();
   const { language } = useLanguage();
+  const { overal } = useOveral();
 
   return (
     <>
-      <BlockLists title="necessary" lang={language} list={list} t={t} />
+      <BlockLists title="necessary" list={overal} lang={language} t={t} img={[img_1, img_2, img_3]} />
       <Br desktop={100} mobile={20} />
       <HelpInform data={helpInformList} lang={language} t={t} />
       <Br desktop={100} mobile={20} />
